58842e00c5133bf092a202bb240fe8348b287561
Gaurav Sharma <kgauravs192@gmail.com>
2024-03-02 11:42:30 +0530

    updated

---
+++ variables.js
@@ -0,0 +1,32 @@
+// variables in typescript
+var x1 = 10;
+console.log(x1);
+var x2 = 10.9;
+console.log(x2);
+var x3 = "mike";
+console.log(x3);
+var x4 = true;
+console.log(x4);
+var x5 = 'stallin';
+// x5=`gaurav`
+console.log(x5);
+// json object
+var obj = {
+    "name": "mike",
+    "age": 30
+};
+console.log(obj.name, obj.age);
+console.log(obj.name);
+// create array in ts
+var arr = [10, 12, 14, 16, 18];
+console.log(arr);
+console.log(arr[0]);
+console.log(arr[1]);
+console.log(arr[2]);
+console.log(arr[3]);
+console.log(arr[4]);
+// function 
+function addNum(x1, x2) {
+    console.log(x1 + x2);
+}
+addNum(10, 20);
